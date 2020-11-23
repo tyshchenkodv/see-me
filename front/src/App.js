459e7a266ec9d3e90fed533912066325e852804a
TheAlterBlack <alterblackoffical@gmail.com>
2020-11-23 21:38:07 +0200

    first commit

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component{
+  render() {
+    return (
+        <div className='App'>
+          <p>New social media - <span>SeeMe</span></p>
+        </div>
+    );
+  }
+}
