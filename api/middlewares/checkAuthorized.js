20e50103efcb2c6bd4e42ce8a7f8bfa70a478e56
Dmitry <45153082+tyshchenkodv@users.noreply.github.com>
2021-02-09 19:53:02 +0200

    Add acl (#13)

--- api/middlewares/checkAuthorized.js
+++ api/middlewares/checkAuthorized.js
@@ -3,18 +3,29 @@ const db = DIContainer.resolve('db');
 const ForbiddenExceptions = DIContainer.resolve('forbiddenExceptions');
 const UnauthorizedException = DIContainer.resolve('unauthorizedException');
 
-module.exports = (tableName, columnName) => {
+module.exports = (rules) => {
     return async (req, res, next) => {
-        try {
-            const post = await db(tableName).
-                select('*').
-                where(columnName, req.user.id).
-                andWhere('id', req.params.id).first();
+        const { user } = req;
+        const { id } = req.params;
 
-            if(!post){
-                return next(new ForbiddenExceptions());
+        try {
+            if (rules.some( async (rule) => {
+                if (!rule.own) {
+                    return user.permissions.includes(rule.permissions);
+                }
+                const post = await db(rule.own.table).
+                select('*').
+                where(rule.own.column, user.id).
+                andWhere('id', id).first();
+
+                if(!post){
+                    return next(new ForbiddenExceptions());
+                }
+
+                return post && post[rule.own.column] === id;
+            })) {
+                return next();
             }
-            next();
         } catch (error){
             return next(new UnauthorizedException(error));
         }
