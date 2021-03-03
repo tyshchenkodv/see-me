0dd287d933640c4e5bdf1a9f615dd5d5303bf885
TheAlterBlack <alterblackoffical@gmail.com>
2021-03-03 19:10:03 +0200

    Add load more in articles list

--- front/src/pages/ArticlesPage/ArticlesPage.jsx
+++ front/src/pages/ArticlesPage/ArticlesPage.jsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import ArticlesListItem from '../../components/ArticlesListItem';
 import { getAllArticles } from './apiCalls';
 import { useQuery } from "react-query";
 import LinearProgress from '@material-ui/core/LinearProgress';
 
 export default function ArticlesPage() {
-    const { data: response, isFetching} = useQuery('articles', () => {
+    const [articles, setArticles] = useState([]);
+    const [visible, setVisible] = useState(5);
+
+    const loadMore = useCallback(() => {
+        setVisible(visible + 5);
+    }, []);
+
+    const {data: response, isFetching} = useQuery('articles', () => {
         const token = window.localStorage.getItem('token');
         return getAllArticles(token);
     });
-    const { posts } = response?.data || [];
+
+    useEffect(() => {
+        if (response) {
+            const {posts} = response?.data || [];
+            setArticles(posts);
+        }
+    }, [isFetching]);
 
     return (
-        !isFetching ? posts.map((post, key) =>
-            <ArticlesListItem article = { post } key={key}/>
-        ) : <LinearProgress />
-    );
+        <>
+            {!isFetching ?
+                <div>
+                    {articles.slice(0, visible).map((article, index) => {
+                        return <ArticlesListItem article={article} key={index}/>
+                    })}
+                    <button onClick={loadMore}
+                            type="button"
+                            className="btn btn-default"
+                            hidden={visible >= articles.length}>Load more
+                    </button>
+                </div>
+                : <LinearProgress/>}
+        </>);
 }
